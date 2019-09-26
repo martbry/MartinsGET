6a49eb40b01ff8fd4ea876e8b31e070f99e2233f
martbry <martin_b91@hotmail.com>
2019-09-26 08:52:34 +0200

    Kommentarer i .HTML- og js-fila

--- Nettsidetest/oblig1/oblig1.js
+++ Nettsidetest/oblig1/oblig1.js
@@ -1,6 +1,10 @@
 // JavaScript source code
 
+//forrigeMeny er, når siden lastes, en tom variabel. Når siden er i bruk holder den på den forrige undermenyen som var åpen
+
 var forrigeMeny;
+
+//Alle hovedmenytaggene har en evenListener som kaller på visUndermeny() når musen hovres over en av lenkene. Eventlisteneren sender det elementet som trigget mouseover som første argument.
 
 document.getElementById("forside").addEventListener("mouseover", visUndermeny);
 document.getElementById("test1").addEventListener("mouseover", visUndermeny);
@@ -10,20 +14,34 @@ document.getElementById("test4").addEventListener("mouseover", visUndermeny);
 document.getElementById("hovedmeny").addEventListener("mouseover", visUndermeny);
 
 
+//visUndermeny(par1) tar i mot elementet som trigger mouseover fra EventListerene over
+
 function visUndermeny(par1) {
+
+    //Denne if-setningen sjekke først om forrigeMeny allerede holder på en tidligere meny og skjuler den dersom den gjør det
 
     if (forrigeMeny != undefined) {
         forrigeMeny.style.display = "none";
     }
 
+    //Denne if-setningen passer på at dersom man hovrer musa over en del av navbaren (hovedmenyen)
+    //helt på toppen av siden som ikke har noen lenker, så stopper koden her(etter den har skjult forrige meny)
+
     if (par1.target.id == "hovedmeny") {
         return;
     }
 
-    forrigeMeny = par1.target;
+    //id blir tilordnet samme verdi som IDen til lenken som trigger undermenyen. Det er denne som brukes til å passe på at det er den rette undermenyen som blir vist
+    //sistetegn settes til å være siste tegnet i den ovennevnte IDen.
 
     var id = par1.target.id;
     var sisteTegn = id.charAt(id.length - 1);
+
+    //Denne if-blokka er dels hardkodet. Hvis sistetegn er lik "e" (altså siste tegnet i IDen til elementet som trigget denne funksjonen), så vet jeg at det er
+    //elementet med ID "forside". Da skal undermeny "u1" vises. Og forrigeMeny tilordnes den undermenyen.
+
+    //I alle andre tilfeller tar jeg siste tegn i IDen (som alltid er et tall (test1, test2 osv.)) og viser undermenyen med id som starter på "u" (for undermeny) og slutter på dette tallet pluss 1
+    //slik at test1 åpner u2, test2 åpner u3 osv. forrigemeny tilordnes så denne undermenyen.
 
     if (sisteTegn == "e") {
         document.getElementById("u1").style.display = "block";
@@ -39,17 +57,6 @@ function visUndermeny(par1) {
 
 }
 
-function skjul(par1) {
-    par1.style.display = "none";
-}
-
-
-                            //function visUndermeny(par1) {
-                            //    while (par1.onmouseover || document.getElementById("undermeny").onmouseover) {
-                            //        document.getElementById("undermeny").style.display = "block";
-                            //    }
-                            //}
-
-                            //function skjulUndermeny() {
-                            //    document.getElementById("undermeny").style.display = "none";
-                            //}
+//function skjul(par1) {
+//    par1.style.display = "none";
+//}
