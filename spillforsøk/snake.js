16dc1c7c4121e4a75425dc9a78fc5595b99b38d2
martbry <martinb@getacademy.com>
2019-10-07 15:04:27 +0200

    x

--- spillforsøk/snake.js
+++ spillforsøk/snake.js
@@ -3,14 +3,13 @@
 var canv = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var slange = [50, 500, 50, 50]
-
+var piltast = "hoyre";
 
 var stopp =
-    setInterval(function tegn(){
         ctx.clearRect(0, 0, canv.width, canv.height);
         ctx.strokeRect(50, 50, 700, 500);
         ctx.strokeRect(slange[0], slange[1], slange[2], slange[3]);
-        update();
+        update(piltast);
     }, 200);
 
 //function tegn() {
@@ -20,16 +19,34 @@ var stopp =
 //}
 
 function update(retning) {
-    if (slange[0] < canv.width - 50) {
+    if (retning == "hoyre" && slange[0] < canv.width - 50) {
         slange[0] += 50;
         return;
+    } else if (retning == "venstre" && slange[0] >= 50) {
+        slange[0] -= 50;
+    } else if (retning == "ned" && slange[1] < canvas.height - 50) {
+        slange[1] += 50;
+    } else if (retning == "opp" && slange[1] >= 50) {
+        slange[1] -= 50;
     } else {
         console.log("hallo");
         clearInterval(stopp);
     }
-    
-    
 }
+
+function endrePiltast(trykket) {
+    if (trykket.keyCode == 37) {
+        piltast = "venstre";
+    } else if (trykket.keyCode == 38) {
+        piltast = "opp";
+    } else if (trykket.keyCode == 39) {
+        piltast = "hoyre";
+    } else if (trykket.keyCode == 40) {
+        piltast = "ned";
+    }
+}
+
+document.addEventListener("keydown", endrePiltast);
 
 //tegn();
 //start();
